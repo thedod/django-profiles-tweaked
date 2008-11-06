e5d784611ca7b295abb44694fc510f48519af8c8
James Bennett <james@b-list.org>
2008-11-06 08:09:07 -0600

    Bump to 0.2

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='django-profiles',
-      version='0.1',
+      version='0.2',
       description='User-profile application for Django',
       author='James Bennett',
       author_email='james@b-list.org',
       url='http://www.bitbucket.org/ubernostrum/django-profiles/wiki/',
-      download_url='http://www.bitbucket.org/ubernostrum/django-profiles/get/v0.1.gz',
+      download_url='http://www.bitbucket.org/ubernostrum/django-profiles/get/v0.2.gz',
       packages=['profiles'],
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
