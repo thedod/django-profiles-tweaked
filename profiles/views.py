c7e86d3ee4cdd8baff9613bcd7169dff6867d539
James Bennett <james@b-list.org>
2009-01-12 07:36:42 -0600

    Stylistic import rearrangement.

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,8 @@ Views for creating, editing and viewing site-specific user profiles.
 
 """
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 from django.http import Http404
@@ -11,9 +13,6 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.generic.list_detail import object_list
-
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 
 from profiles import utils
 
